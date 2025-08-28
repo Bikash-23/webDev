c685b644bbe0f49ef7460b7165f950b7375e832b
Bikash-23 <paribikash23@gmail.com>
2025-08-28 22:25:38 +0530

    ok

--- javaScript/01_Basic/03_conversionOperation.js
+++ javaScript/01_Basic/03_conversionOperation.js
@@ -7,17 +7,17 @@ let score = "33abc"
 //undefiend = NaN
 //"string" = NaN
 
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 let isLogedIn = "NaN"
 let booleanisLogedIn = Boolean(isLogedIn)
-console.log(booleanisLogedIn);
+// console.log(booleanisLogedIn);
 
 //1 = true; 0 = false
 //"" = false
@@ -25,7 +25,33 @@ console.log(booleanisLogedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-
+//***********************Operation ************************
+let value =3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3/2);
+// console.log(3**2);
+// console.log(5%2);
+// console.log(""+2+2);
+ let str1 = "Hello"
+ let str2 = " Bikash"
+ str1 +=str2
+ console.log(str1);
+ console.log("1"+2);
+ console.log(1+"2");
+ console.log("1"+2+2);
+ console.log(1+2+"2");
+ console.log(+true);
+ console.log(+"");
+ let gameCounter =100
+ 
+ console.log(gameCounter++);
+ let gc = 123
+ console.log(++gc);
+ 

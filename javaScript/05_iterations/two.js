90df758003cbb106b3f5a7768de05cd8c871a51a
Bikash-23 <paribikash23@gmail.com>
2025-11-09 22:43:40 +0530

    Js

---
+++ javaScript/05_iterations/two.js
@@ -0,0 +1,28 @@
+// while 
+
+// let index = 0
+// while (index <= 10) {
+//      console.log(`Value of index is ${index}`);
+//     index +=2
+// }
+
+// let myArray = ['flash','batman','superman']
+// let arr= 0
+// while(arr<myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+// do while
+
+let score1 = 1
+do {
+   console.log(`Score is ${score1}`);
+    score1++
+} while (score1<=10);
+
+let score2 = 11
+do {
+   console.log(`Score is ${score2}`);
+    score2++
+} while (score2<=10);
